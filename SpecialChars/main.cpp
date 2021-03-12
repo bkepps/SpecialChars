6c730a56f6321c1e7d21b77733ca7ca55406cef3
bkepps <benskepp@gmail.com>
2021-03-12 13:39:49 -0600

    more responsive seeming

--- SpecialChars/main.cpp
+++ SpecialChars/main.cpp
@@ -3,6 +3,7 @@
 
 void sendKey(BYTE key) {
 	keybd_event(0, key, KEYEVENTF_UNICODE, NULL);
+	Sleep(500);
 }
 
 int main() {
@@ -38,8 +39,7 @@ int main() {
 			}
 			if (GetAsyncKeyState('S') & 0x8000) {
 				sendKey('ß');
-			}
-			Sleep(500);
+			}			
 		}
 		Sleep(100);
 	}
