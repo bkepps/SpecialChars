4226fdf364acd7180d766c386afe7c89459a5881
bkepps <benskepp@gmail.com>
2021-03-11 21:31:35 -0600

    It works, a bit sketchy tho

--- SpecialChars/main.cpp
+++ SpecialChars/main.cpp
@@ -2,15 +2,32 @@
 #include <Windows.h>
 
 int main() {
-	BYTE keyStatus[256];
+	UINT error;
 
-	INPUT inputs[4];
+	INPUT inputs[7];
 	ZeroMemory(inputs, sizeof(inputs));
 
-	for (;;) {
-		GetKeyboardState((LPBYTE)&keyStatus);
-		if (keyStatus[VK_LSHIFT] && keyStatus[VK_CONTROL] && keyStatus[VK_MENU]) {
+	inputs[0].type = INPUT_KEYBOARD;
+	inputs[0].ki.dwFlags = KEYEVENTF_UNICODE;
+	inputs[0].ki.wScan = 0x00C4;	//A with umlauts
 
+	inputs[1].type = INPUT_KEYBOARD;
+	inputs[1].ki.dwFlags = KEYEVENTF_UNICODE;
+	inputs[1].ki.wScan = 0x00E4;	//a with umlauts
+
+
+	while (1) {
+		if ((GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState(VK_MENU) & 0x8000)) {		//CTLR and ALT pressed
+			if (GetAsyncKeyState(0x41) & 0x8000) {								//A key
+				if (GetAsyncKeyState(VK_LSHIFT) & 0x8000) {						//Shift pressed
+					error = SendInput(1, &inputs[0], sizeof(INPUT));
+					std::cout << std::hex << error;
+				}
+				else {															//Shift not pressed
+					SendInput(1, &inputs[1], sizeof(INPUT));
+				}
+			}
+			Sleep(500);
 		}
 	}
 
